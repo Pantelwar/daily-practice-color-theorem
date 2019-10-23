53bb97fdf4323f0ee2e0b95693f09410c7227843
Hell Harv <gandharv.pantelwar@gmail.com>
2019-10-24 00:23:07 +0530

    #2

--- python/color-theorem.py
+++ python/color-theorem.py
@@ -1,6 +1,6 @@
 from itertools import permutations
 
-areas = ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8"]
+areas = ["A1", "A2", "A3", "A4", "A5", "A6", "A7"]#, "A8"]
 colors = ["green", "yellow", "blue", "orange"]
 # colors = ["yellow", "blue", "orange","green"]
 # colors = ('orange', 'yellow', 'blue', 'green')
@@ -10,13 +10,13 @@ permsAreas = permutations(areas, len(areas))
 
 neighborPoints = {
     "A1": ["A2", "A4", "A6"],
-    "A2": ["A1", "A3", "A4", "A6", "A7"],
-    "A3": ["A2", "A4", "A5", "A7"],
+    "A2": ["A1", "A3", "A4", "A6", "A7"],#, "A8"],
+    "A3": ["A2", "A4", "A5", "A7"],#, "A8"],
     "A4": ["A1", "A2", "A3", "A5", "A6", "A7"],
     "A5": ["A3", "A4", "A6"],
     "A6": ["A1", "A2", "A4", "A5"],
     "A7": ["A2", "A3", "A4"],
-    "A8": ["A2", "A3"]
+    #"A8": ["A2", "A3"]
 }
 
 totalCombinations = []
@@ -47,12 +47,12 @@ def getResult(assignedColors, areasList, colorsList):
                 for neighborPoint in neighborPoints[area]:
                     if assignedColors.has_key(neighborPoint):
                         if assignedColors[neighborPoint] == color:
-                            print "area:", area, "neighbour:", neighborPoint, "color:", color
+                            # print "area:", area, "neighbour:", neighborPoint, "color:", color
                             found = True
                             break
                 if found:
                     continue
-                print "area:", area, "color:", color
+                # print "area:", area, "color:", color
                 assignedColors[area] = color
                 break
     return assignedColors
@@ -65,12 +65,12 @@ for permsArea in permsAreas:
         print colorsList, areasList
         for i in range(0, len(areasList)):
             for j in range(0, len(colorsList)):
-                finalResult[areas[i]] = colorsList[j]
+                finalResult[areasList[i]] = colorsList[j]
 
                 finalResult = getResult(finalResult, areasList, colorsList)
 
                 print finalResult
-                if len(finalResult) != len(areas):
+                if len(finalResult) != len(areasList):
                     print "Discarded result:", finalResult
                 else:
                     if not finalResult in totalCombinations:
@@ -83,4 +83,28 @@ for combinations in totalCombinations:
 
 print len(totalCombinations)
 
-
+# print "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+# totalCombinations = []
+# finalResult = {}
+# colors = ['orange', 'yellow', 'green', 'blue']
+# areas = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8']
+# for i in range(0, len(areas)):
+#     for j in range(0, len(colors)):
+#         finalResult[areas[i]] = colors[j]
+#         print "#1", finalResult
+#
+#         finalResult = getResult(finalResult, areas, colors)
+#
+#         print finalResult
+#         if len(finalResult) != len(areas):
+#             print "Discarded result:", finalResult
+#         else:
+#             if not finalResult in totalCombinations:
+#                 totalCombinations.append(finalResult)
+#         finalResult = {}
+#
+# for combinations in totalCombinations:
+#     print combinations
+#
+# print len(totalCombinations)
+#
